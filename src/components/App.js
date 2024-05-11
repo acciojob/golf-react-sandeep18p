28f2f8103ad377265e2a7126e9f098271f173dd4
sandeep18p <sandeep.pansari98@gmail.com>
2024-05-11 13:19:05 +0530

    one

--- src/components/App.js
+++ src/components/App.js
@@ -11,11 +11,23 @@ class App extends Component {
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
+        this.handleKeyDown = this.handleKeyDown.bind(this);
     };
 
     buttonClickHandler() {
-   
+      this.setState({renderBall:true});
    }
+
+   handleKeyDown(event){
+    if(this.state.renderBall === true && event.key === "ArrowRight"){
+     this.setState((prevState)=>({
+        ballPosition:{
+          left: parseInt(prevState.ballPosition.left) + 5 + "px"
+        }
+     }))
+    }
+   }
+
     renderBallOrButton() {
 		if (this.state.renderBall) {
 		    return <div className="ball" style={this.state.ballPosition}></div>
@@ -26,7 +38,7 @@ class App extends Component {
 
     // bind ArrowRight keydown event
     componentDidMount() {
-      
+      document.addEventListener("keydown", this.handleKeyDown);
     }
 
     render() {
